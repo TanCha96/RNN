d476b95c56ad07647e130f1271e9014be2a5c8c8
Tanmay Chanda <35967616+TanCha96@users.noreply.github.com>
2019-08-28 16:53:48 +0530

    Update RNN_words.py

--- RNN_words.py
+++ RNN_words.py
@@ -2,9 +2,6 @@
 A Recurrent Neural Network (LSTM) implementation example using TensorFlow..
 Next word prediction after n_input words learned from text file.
 A story is automatically generated if the predicted word is fed back as input.
-
-Author: Rowel Atienza
-Project: https://github.com/roatienza/Deep-Learning-Experiments
 '''
 
 from __future__ import print_function
